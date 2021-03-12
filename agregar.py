7dc258e4ad2c6071bd26223a7bed4541717de62a
Jose Gomez <chkm64.goma@gmail.com>
2021-03-12 11:20:25 -0500

    Agregando todos los parametros permitidos para la conexión

--- agregar.py
+++ agregar.py
@@ -1,6 +1,6 @@
 import routeros_api
 
-connection = routeros_api.RouterOsApiPool('', username='', password='', port=8728, plaintext_login=True)
+connection = routeros_api.RouterOsApiPool('', username='', password='', plaintext_login=True, port=8728, use_ssl=False, ssl_verify=False, ssl_verify_hostname=False, ssl_context=None)
 api = connection.get_api()
 ruta_mikrotik =  api.get_resource('/ip/firewall/address-list')
 lineas = ruta_mikrotik.add(address="10.10.10.10", list="address-list")
--- buscar.py
+++ buscar.py
@@ -1,7 +1,7 @@
 
 import routeros_api
 
-connection = routeros_api.RouterOsApiPool('45.231.168.170', username='amendez', password='/**S1st3m4s**/', plaintext_login=True, port=50001)
+connection = routeros_api.RouterOsApiPool('', username='', password='', plaintext_login=True, port=8728, use_ssl=False, ssl_verify=False, ssl_verify_hostname=False, ssl_context=None)
 api = connection.get_api()
 ruta_mikrotik = api.get_resource('/ip/firewall/address-list')
 result = ruta_mikrotik.get(address="10.10.10.10")
