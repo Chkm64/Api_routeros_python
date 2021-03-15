6b65ddc3c1df42495d07e03375fc639e03e9cec1
Jose Gomez <chkm64.goma@gmail.com>
2021-03-15 07:37:36 -0500

    Consulta de logs general

---
+++ log.py
@@ -0,0 +1,15 @@
+import routeros_api
+
+connection = routeros_api.RouterOsApiPool('', username='', password='', plaintext_login=True, port=8728, use_ssl=False, ssl_verify=False, ssl_verify_hostname=False, ssl_context=None)
+api = connection.get_api()
+ruta_mikrotik =  api.get_resource('/log')
+logs_mikro = ruta_mikrotik.get()
+
+# Imprimir toda la consulta
+print(logs_mikro)
+
+# Imprimir datos 1 x 1
+for log un logs_mikro
+  print(log)
+
+connection.disconnect()
